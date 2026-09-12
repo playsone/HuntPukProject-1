957e7ad0d9933318085f88e6ca7b1c204145f5c9
playsone <oammanat9091@gmail.com>
2026-09-12 16:18:45 +0700

    fix: update API endpoint to remove unnecessary path

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Constants {
-  public readonly API_ENDPOINT: string = 'https://huntpuk-api.onrender.com/api';
+  public readonly API_ENDPOINT: string = 'https://huntpuk-api.onrender.com';
 }
